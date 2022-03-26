5e99e97350082f70a19c3316481f65c9241aa0a9
HUBERT <hubertR2019@gmail.com>
2022-03-26 15:17:38 +0100

    CRUD update

--- Kalendarz/Classes/Event.cs
+++ Kalendarz/Classes/Event.cs
@@ -8,7 +8,7 @@ namespace Kalendarz.Classes
 {
     public class Event
     {
-        public int id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime Created { get; set; }
--- Kalendarz/MainWindow.xaml.cs
+++ Kalendarz/MainWindow.xaml.cs
@@ -22,29 +22,50 @@ namespace Kalendarz
     {
         public MainWindow()
         {
-            
-            using Context myContext=new Context();
-            
-            
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void AddToDatabase_Click(object sender, RoutedEventArgs e)
         {
-            
+            using Context myContext = new Context();
+            Event event1 = new Event();
+            event1.Id = int.Parse(id_wydarzenia.Text);
+            event1.Name = nazwa_wydarzenia.Text;
+            event1.Description = opis_wydarzenia.Text;
+            event1.Created = DateTime.Now;
+            myContext.Events.Add(event1); 
+            myContext.SaveChanges();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void RemoveFromDatabase_Click(object sender, RoutedEventArgs e)
         {
-
+            using (Context myContext = new Context())
+            {
+                int id = int.Parse(id_do_usuniecia.Text);
+                var blogs = myContext.Events.Find(id);
+                myContext.Events.Remove(blogs);
+                myContext.SaveChanges();
+            }
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
             string city = Wpisz_miasto.Text;
             APIConnector apiConnector = new APIConnector();
             Weather weather = new Weather();
-            apiConnector.getWeather(ref weather,city);
+            apiConnector.getWeather(ref weather, city);
 
             id_miasta.Text = weather.id_stacji.ToString();
-            temp_miasta.Text=weather.temperatura.ToString();
-            wilg_miasta.Text=weather.wilgotnosc_wzgledna.ToString();
+            temp_miasta.Text = weather.temperatura.ToString();
+            wilg_miasta.Text = weather.wilgotnosc_wzgledna.ToString();
+        }
+        private void EditRecord_Click(object sender, RoutedEventArgs e)
+        {
+            using Context myContext = new Context();
+            int id = int.Parse(id_wydarzenia_Edit.Text);
+            var blogs = myContext.Events.Find(id);
+            blogs.Name = nazwa_wydarzenia_Edit.Text;
+            blogs.Description = opis_wydarzenia_Edit.Text;
+            blogs.Created = DateTime.Now;
+            myContext.SaveChanges();
         }
     }
 }
+
